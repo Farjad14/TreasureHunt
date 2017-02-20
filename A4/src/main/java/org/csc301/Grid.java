30b1dd35bab89182fe7a4c7f68eac24fbf33bab6
Osama Alam <osama.alam@mail.utoronto.ca>
2017-02-20 09:31:39 -0500

    treasureHunt

--- A4/src/main/java/org/csc301/Grid.java
+++ A4/src/main/java/org/csc301/Grid.java
@@ -39,6 +39,39 @@ public class Grid {
 		// For each map position (i,j) you need to generate a Node with can be navigable or it may belong to an island
 		// You may use ideas from Lab3 here.
 		// Don't forget to generate the location of the boat and of the treasure; they must be on navigable waters, not on the land!
+		
+		//create the map and place the boat
+		map = new Node[height][width];
+		Random rand = new Random();
+		int boatX = rand.nextInt(width);
+		int boatY = rand.nextInt(height);
+		boat = new Node(true, boatX, boatY);
+		map[boatX][boatY] = boat;
+		
+		int treasX = rand.nextInt(width);
+		int treasY = rand.nextInt(height);
+		
+		//find a spot to place the treasure
+		while (map[treasX][treasY] != null){
+			treasX = rand.nextInt(width);
+			treasY = rand.nextInt(height);
+		}
+		treasure = new Node(true, treasX, treasY);
+		map[treasX][treasY] = treasure;
+		
+		//Fill the grid with island or navigable
+		for(int i=0; i<width;i++){
+			for(int j=0; j<height;j++){
+				if (map[i][j] != null){
+					if(rand.nextInt(99)<percent){
+						map[i][j] = new Node(false, i, j);
+					}
+					else{
+						map[i][j] = new Node(true, i, j);
+					}
+				}
+			}
+		}
 	}
 
 	public String drawMap() {
@@ -97,9 +130,57 @@ public class Grid {
 	}
 	
 	private ArrayList<Node> getNeighbours(Node node) {
-		return null;
 		// each node has at most 8 neighbours
 		// Lab3 may be useful here as well
+		ArrayList<Node> neighbours = new ArrayList<Node>();
+		boolean n,s,e,w;
+		n = (node.gridY - 1) >=0;
+		s = (node.gridY + 1) < height;
+		e = (node.gridX + 1) < width;
+		w = (node.gridX - 1) >= 0;
+		
+		//north
+		if((node.gridY - 1) >=0){
+			neighbours.add(map[node.gridY-1][node.gridX]);
+		}
+		
+		//north west
+		if(((node.gridY - 1) >=0) && ((node.gridX - 1) >= 0)){
+			neighbours.add(map[node.gridY-1][node.gridX-1]);
+		}
+		
+		//north east
+		if(((node.gridY + 1) < height) && ((node.gridX + 1) < width)){
+			neighbours.add(map[node.gridY-1][node.gridX-1]);
+		}
+		
+		//south east
+		if(((node.gridX + 1) < width) && ((node.gridY + 1) < height)){
+			neighbours.add(map[node.gridY +1][node.gridX +1]);
+		}
+		
+		//south
+		if((node.gridY + 1) < height){
+			neighbours.add(map[node.gridY+1][node.gridX]);
+		}
+		
+		//south west
+		if(((node.gridY + 1) < height) && ((node.gridX - 1) >= 0)){
+			neighbours.add(map[node.gridY][node.gridX]);
+		}
+		
+		//east
+		if((node.gridX + 1) < width){
+			neighbours.add(map[node.gridY][node.gridX+1]);
+		}
+		
+		//west
+		if((node.gridX - 1) >= 0){
+			neighbours.add(map[node.gridY][node.gridX+1]);
+		}
+		
+		
+		return neighbours;
 	}
 
 	private int getDistance(Node nodeA, Node nodeB) {
--- A4/src/main/java/org/csc301/TreasureHunt.java
+++ A4/src/main/java/org/csc301/TreasureHunt.java
@@ -19,20 +19,52 @@ public class TreasureHunt {
 
 	public TreasureHunt() {
 		// The default constructor
+		islands = new Grid();
+		sonars = DEFAULT_SONARS;
+		range = DEFAULT_RANGE;
+		state = "STARTED";
 	}
 
 	public TreasureHunt(int height, int width, int landPercent, int sonars,
 			int range) {
 		// The constructor thatuses parameters
+		this.islands = new Grid(width, height, landPercent);
+		this.sonars = sonars;
+		this.range = range;
+		this.state = "STARTED";
 	}
 
-	private void processCommand(String command) throws HeapFullException,
+	public void processCommand(String command) throws HeapFullException,
 			HeapEmptyException {
 		// The allowed commands are: 
 		// SONAR to drop the sonar in hope to detect treasure
 		// GO direction to move the boat in some direction
 		// For example, GO NW means move the boat one cell up left (if the cell is navigable; if not simply ignore the command) 
+	if (!(state.equals("OVER"))){
+		
+		//SONAR command
+		if (command.equals("SONAR")){
+			//Check if we have a sonar avail
+			if(sonars > 0){
+				//Find the booty!!
+				Node booty = islands.getTreasure(range);
+				sonars--;
+				
+				// find treasure and run a star alg to the booty(treasure)!
+				if(booty != null){
+					islands.findPath(islands.getBoat(), booty);
+					path = islands.retracePath(islands.getBoat(), booty);
+					state = "OVER";
+				}
+			}
+		}
 	}
+	else{
+		return;
+	}
+	
+	}
+	
 
 	public int pathLength() {
 		if (path == null)
@@ -47,6 +79,12 @@ public class TreasureHunt {
 	public void play(String pathName) throws FileNotFoundException,
 			HeapFullException, HeapEmptyException {
 		// Read a batch of commands from a text file and process them.
+		Scanner sc = new Scanner(new File(pathName));
+		while (sc.hasNextLine()){
+			String cmd = sc.nextLine();
+			processCommand(cmd);
+		}
+		sc.close();
 	}
 
 }
